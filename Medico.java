7b3699e46d6117937868a15964334aa2417176e7
PauloHPS1991 <30605988+PauloHPS1991@users.noreply.github.com>
2020-04-09 14:58:06 -0300

    Add files via upload

---
+++ Medico.java
@@ -0,0 +1,75 @@
+package com.example.appamd;
+
+public class Medico {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private String especialidade;
+
+    public Medico() {
+    }
+
+    public Medico(int id, String nome, String telefone, String email, String endereco, String especialidade) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+        this.especialidade = especialidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
