ab7c0d1da40162d5cb08fa0189d707baeee1e3e2
PauloHPS1991 <30605988+PauloHPS1991@users.noreply.github.com>
2020-04-09 15:29:37 -0300

    Add files via upload

---
+++ testa_cadastro.php
@@ -0,0 +1,8 @@
+<form action="cadastrar.php" method="POST">
+	Nome: <input type="text" name="nome_app" maxlength="30"><br />
+	Telefone: <input type="text" name="telefone_app" maxlength="11"><br />
+	E-mail: <input type="text" name="email_app" maxlength="30"><br />
+	Cartao: <input type="text" name="cartao_app"maxlength="20"><br />
+	
+	<input type="submit" value="Cadastrar">
+</form>
---
+++ testa_cadastrom.php
@@ -0,0 +1,9 @@
+<form action="cadastram.php" method="POST">
+	Nome: <input type="text" name="nome_app" maxlength="30"><br />
+	Telefone: <input type="text" name="telefone_app" maxlength="11"><br />
+	E-mail: <input type="text" name="email_app" maxlength="30"><br />
+	Endereco: <input type="text" name="endereco_app" maxlength="50"><br />
+	Especialidade: <input type="text" name="especialidade_app" maxlength="20"><br />
+	
+	<input type="submit" value="Cadastrar">
+</form>
